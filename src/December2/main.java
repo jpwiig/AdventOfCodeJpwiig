0766eafde643f056b0d7f49200169127aa25ea21
jpwiig <jpwiig@gmail.com>
2022-12-02 13:39:13 +0100

    some for luke 2

---
+++ src/December2/main.java
@@ -0,0 +1,13 @@
+package December2;
+
+public class main {
+    public static void main(String[] args) {
+        int[]scores = {1,2,3}; //paper = Y, rock = x, sissors = Z
+        char[]param = {'Y','X','Z'};
+        int score = 0;
+        char input = 'a';
+        for (int i= 0; i < scores.length -1; i++){
+
+        }
+    }
+}
